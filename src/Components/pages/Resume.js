8f112570be33ed0f880a5b9faa139a648f2ebe23
Andrew Ahn <andrewahn21@gmail.com>
2024-01-07 17:28:13 -0800

    Added resume.

--- src/Components/pages/Resume.js
+++ src/Components/pages/Resume.js
@@ -6,6 +6,9 @@ export default function Resume() {
     return (
         <div>
             <h1 className='resume'>RESUME</h1>
+            <div>
+                <iframe src="resume.pdf" width="80%" height="500px" />
+            </div>
         </div>
     )
 };
