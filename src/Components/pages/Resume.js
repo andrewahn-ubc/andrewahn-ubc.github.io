b95d61d83db889332916ee1d201bc56ad1c6c5ce
Andrew Ahn <andrewahn21@gmail.com>
2024-01-07 18:26:19 -0800

    Centered resume and added background color.

--- src/Components/pages/Resume.js
+++ src/Components/pages/Resume.js
@@ -4,11 +4,8 @@ import Footer from '../Footer'
 
 export default function Resume() {
     return (
-        <div>
-            <h1 className='resume'>RESUME</h1>
-            <div>
-                <iframe src="resume.pdf" width="80%" height="500px" />
-            </div>
+        <div className='resume'>
+            <iframe className='document' src="resume.pdf" width="70%" height="500px" />
         </div>
     )
 };
