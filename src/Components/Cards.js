93e9ab85f6b3eec2376390c2960cee56aef878ae
Andrew Ahn <andrewahn21@gmail.com>
2024-01-29 01:52:22 -0800

    Added OurBoard and SSW, Updated Resume.

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -15,19 +15,31 @@ function Cards() {
              <div className="cards__wrapper">
                 <ul className="cards__items">
                     <CardItem  
-                        src='images/newWordleScreenshot.png'
-                        text='Wordle Clone!'
-                        path="https://wordleclone-rofc7pu9f-andrewahn-ubcs-projects.vercel.app/"
+                        src='images/logo.png'
+                        text='OurBoard'
+                        path="https://devpost.com/software/ourboard "
                         target='_blank'
                     />
+                    <CardItem  
+                        src='images/study-sessions-wrapped-page.png'
+                        text='Study Sessions Wrapped'
+                        path="https://github.com/andrewahn-ubc/study-sessions-wrapped-v3"
+                        target='_blank'
+                    />  
                     <CardItem  
                         src='images/connections.png'
                         text='NYT Connections Clone'
                         path="https://connections-nyt.vercel.app/"
                         target='_blank'
-                    />
+                    />  
                 </ul>
                 <ul className="cards__items">
+                    <CardItem  
+                        src='images/newWordleScreenshot.png'
+                        text='Wordle Clone!'
+                        path="https://wordleclone-rofc7pu9f-andrewahn-ubcs-projects.vercel.app/"
+                        target='_blank'
+                    />
                     <CardItem  
                         src='images/Screenshot 2023-12-07 085947.png'
                         text='Personal Website'
