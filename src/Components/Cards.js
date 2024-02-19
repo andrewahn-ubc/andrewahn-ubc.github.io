4baf806e0152a3bd5df3d131a1ca5b61b77a8628
Andrew Ahn <andrewahn21@gmail.com>
2024-02-18 17:14:20 -0800

    Update: removed redundancies in footer & cards.

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,62 +1,62 @@
 import React from 'react'
 import CardItem from '../Subcomponents/CardItem';
 import './Cards.css';
-import { Link } from 'react-router-dom';
 import { useBrightnessContext } from '../Hooks/useBrightnessContext';
 
-
+// Reusable card component for displaying personal projects & meals.
 function Cards() {
     const { brightness } = useBrightnessContext()
 
-  return (
-    <div className={brightness == 'light' ? 'cards' : 'cards-dark'}>
-        <h1>My projects:</h1>
-           <div className="cards__container">
-             <div className="cards__wrapper">
-                <ul className="cards__items">
-                    <CardItem  
-                        src='images/logo.png'
-                        text='OurBoard'
-                        path="https://devpost.com/software/ourboard "
-                        target='_blank'
-                    />
-                    <CardItem  
-                        src='images/study-sessions-wrapped-page.png'
-                        text='Study Sessions Wrapped'
-                        path="https://github.com/andrewahn-ubc/study-sessions-wrapped-v3"
-                        target='_blank'
-                    />  
-                    <CardItem  
-                        src='images/connections.png'
-                        text='NYT Connections Clone'
-                        path="https://connections-nyt-25cdfbc7150a.herokuapp.com/"
-                        target='_blank'
-                    />  
-                </ul>
-                <ul className="cards__items">
-                    <CardItem  
-                        src='images/newWordleScreenshot.png'
-                        text='Wordle Clone!'
-                        path="https://wordleclone-3314286f69fb.herokuapp.com/"
-                        target='_blank'
-                    />
-                    <CardItem  
-                        src='images/new-website.png'
-                        text='Personal Website'
-                        path="https://github.com/andrewahn-ubc/first-react-website"
-                        target='_blank'
-                    />
-                    <CardItem  
-                        src='images/fresherPhoto4.png'
-                        text='Fresher'
-                        path="https://github.com/andrewahn-ubc/Fresher"
-                        target='_blank'
-                    />
-                </ul>
+    return (
+        <div className={brightness == 'light' ? 'cards' : 'cards-dark'}>
+            <h1>My projects:</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem  
+                            src='images/logo.png'
+                            text='OurBoard'
+                            path="https://devpost.com/software/ourboard "
+                            target='_blank'
+                        />
+                        <CardItem  
+                            src='images/study-sessions-wrapped-page.png'
+                            text='Study Sessions Wrapped'
+                            path="https://github.com/andrewahn-ubc/study-sessions-wrapped-v3"
+                            target='_blank'
+                        />  
+                        <CardItem  
+                            src='images/connections.png'
+                            text='NYT Connections Clone'
+                            path="https://connections-nyt-25cdfbc7150a.herokuapp.com/"
+                            target='_blank'
+                        />  
+                    </ul>
+
+                    <ul className="cards__items">
+                        <CardItem  
+                            src='images/newWordleScreenshot.png'
+                            text='Wordle Clone!'
+                            path="https://wordleclone-3314286f69fb.herokuapp.com/"
+                            target='_blank'
+                        />
+                        <CardItem  
+                            src='images/new-website.png'
+                            text='Personal Website'
+                            path="https://github.com/andrewahn-ubc/first-react-website"
+                            target='_blank'
+                        />
+                        <CardItem  
+                            src='images/fresherPhoto4.png'
+                            text='Fresher'
+                            path="https://github.com/andrewahn-ubc/Fresher"
+                            target='_blank'
+                        />
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Cards
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,21 +7,19 @@ function Footer() {
   return (
     <div className='footer-container'>
         <section className="footer-farewell">
-            <p className="footer-farewell-message">
+            <p>
                 Thanks for visiting!
             </p>
         </section>
 
-        <section className="social-media">
-            <div className="social-media-wrap">
-
-                <small className="website-rights"> 
-                    <i class="fa-regular fa-copyright"></i> 
-                    Andrew Ahn 2023
-                </small>
-
-            </div>
-        </section>
+        
+    <div className="website-rights">
+        <small> 
+            <i class="fa-regular fa-copyright"></i> 
+            Andrew Ahn 2023
+        </small>
+    </div>
+        
     </div>
   )
 }
