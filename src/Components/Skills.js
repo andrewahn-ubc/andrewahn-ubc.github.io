a72317b7f008a3903dd6218ab3951fd735b92bad
Andrew Ahn <andrewahn21@gmail.com>
2024-03-08 17:14:50 -0800

    Content: added logos to my tech stack

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -12,24 +12,25 @@ const Skills = () => {
             <div className={brightness === 'light' ? 'skills' : 'skills-dark'} >
                 <div className={brightness === 'light' ? "skills-column" : "skills-column-dark"} >
                     <h1>Languages</h1>
-                    <p>JavaScript</p>
-                    <p>Java</p>
-                    <p>C/C++</p>
-                    <p>Python</p>
+                    <p>JavaScript &nbsp; <img src={"images/javascript.png"} alt="Loading" className="techLogo"></img></p>
+                    <p>Java  &nbsp; <img src={"images/java.png"} alt="Loading" className="techLogo"></img></p>
+                    <p>C/C++ &nbsp; <img src={"images/cpp.png"} alt="Loading" className="techLogo"></img></p>
+                    <p>Python &nbsp; <img src={"images/python.png"} alt="Loading" className="techLogo"></img></p>
                 </div>
 
                 <div className={brightness === 'light' ? "skills-column" : "skills-column-dark"} >
                     <h1>Backend</h1>
-                    <p>Node</p>
-                    <p>Express</p>
-                    <p>MongoDB</p>
-                    <p>Heroku</p>
+                    <p>Node &nbsp; <img src={"images/node.png"} alt="Loading" className="techLogo"></img></p>
+                    <p>Express &nbsp; <img src={"images/express.png"} alt="Loading" className="techLogo"></img></p>
+                    <p>MongoDB &nbsp; <img src={"images/mongo.png"} alt="Loading" className="techLogo"></img></p>
+                    <p>Heroku &nbsp; <img src={"images/heroku.png"} alt="Loading" className="techLogo"></img></p>
                 </div>
 
                 <div className={brightness === 'light' ? "skills-column" : "skills-column-dark"} >
                     <h1>Frontend</h1>
-                    <p>React</p>
-                    <p>HTML/CSS</p>
+                    <p>React &nbsp; <img src={"images/react.png"} alt="Loading" className="techLogo"></img></p>
+                    <p>HTML/CSS &nbsp; <img src={"images/html.png"} alt="Loading" className="techLogo"></img>
+                    &nbsp; <img src={"images/css.png"} alt="Loading" className="techLogo"></img></p>
                 </div>
             </div>
         </div>
