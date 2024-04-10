87c81aecaaab1881ee73fdff62c375e058f0f435
Andrew Ahn <andrewahn21@gmail.com>
2024-04-10 13:58:24 -0700

    Fixed card component margin error.

--- src/Components/pages/Food.js
+++ src/Components/pages/Food.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../../App.css"
 import "./Food.css"
-import CardItem from '../../Subcomponents/CardItem'
+import FoodItem from '../../Subcomponents/FoodItem'
 import { useBrightnessContext } from '../../Hooks/useBrightnessContext'
 
 export default function Food() {
@@ -21,34 +21,34 @@ export default function Food() {
             </div>
             
             <ul className="foodList">
-                <CardItem path="/food" src="Food/army_stew.jpg" title="Korean Army Stew" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/burrito_cheese.jpg" title="Cheese-Coated Burrito" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/pork_belly_in_pan.jpg" title="Pork Belly kbbq" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/army_stew.jpg" title="Korean Army Stew" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/burrito_cheese.jpg" title="Cheese-Coated Burrito" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/pork_belly_in_pan.jpg" title="Pork Belly kbbq" date='Jan 01, 2024' details=''/>
             </ul>
 
             <ul className="foodList">
-                <CardItem path="/food" src="Food/first_steak_bite.jpg" title="Flank Steak" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/nacho_bite.jpg" title="Nachos" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/first_steak_bite.jpg" title="Flank Steak" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/nacho_bite.jpg" title="Nachos" date='Jan 01, 2024' details=''/>
             </ul>
 
             <ul className="foodList">
-                <CardItem path="/food" src="Food/dumplings.jpg" title="Dumplings" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/fusilli_carbo.jpg" title="Carbonara" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/shakshukka_closeup.jpg" title="Shaksukka" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/burrito_bowl.jpg" title="Chipotle Bowl" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/dumplings.jpg" title="Dumplings" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/fusilli_carbo.jpg" title="Carbonara" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/shakshukka_closeup.jpg" title="Shaksukka" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/burrito_bowl.jpg" title="Chipotle Bowl" date='Jan 01, 2024' details=''/>
             </ul>
 
             <ul className="foodList">
-                <CardItem path="/food" src="Food/mabo_tofu.jpg" title="Mabo Tofu" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/kfc.jpg" title=" KFC (Korean Fried Chicken)" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/chicken_fried_rice.jpg" title="Crispy Chicken + Fried Rice" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/mabo_tofu.jpg" title="Mabo Tofu" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/kfc.jpg" title=" KFC (Korean Fried Chicken)" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/chicken_fried_rice.jpg" title="Crispy Chicken + Fried Rice" date='Jan 01, 2024' details=''/>
             </ul>
 
             <ul className="foodList">
-                <CardItem path="/food" src="Food/macaroni_meatballs.jpg" title="Pasta + meatballs" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/kimchi_pancake.jpg" title="Kimchi Pancakes" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/oyakodon_bowl.jpg" title="Oyakodon" date='Jan 01, 2024' details=''/>
-                <CardItem path="/food" src="Food/beef_pepper_lunch.jpg" title="Beef Pepper Lunch" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/macaroni_meatballs.jpg" title="Pasta + meatballs" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/kimchi_pancake.jpg" title="Kimchi Pancakes" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/oyakodon_bowl.jpg" title="Oyakodon" date='Jan 01, 2024' details=''/>
+                <FoodItem path="/food" src="Food/beef_pepper_lunch.jpg" title="Beef Pepper Lunch" date='Jan 01, 2024' details=''/>
             </ul>
         </div>
     )
---
+++ src/Subcomponents/FoodItem.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useBrightnessContext } from '../Hooks/useBrightnessContext'
+import "./FoodItem.css"
+
+// Abstract the card component display for personal projects and meals. Yay modular programming!
+function FoodItem(props) {
+    const { brightness } = useBrightnessContext()
+  return (
+    <>
+        <li className="food-item">
+            <Link className={brightness === 'light' ? "food-item-link" : "food-item-link-dark"} to={props.path} target='_blank'>
+                <div className="food-item-info">
+                    <figure className="food-item-pic-wrap" data-category={props.label}>
+                        <img src={props.src} alt="Loading" className="food-item-img" />
+                    </figure>
+                    <div className="food-text">
+                        <h1 className={brightness === 'light' ? "food-item-text" : "food-item-text-dark"}>
+                            {props.title}
+                        </h1>
+                        <h2 className={brightness === 'light' ? "food-item-date" : "food-item-date-dark"}>
+                            {props.date}
+                        </h2>
+                        <h2 className={brightness === 'light' ? "food-item-details" : "food-item-details-dark"}>
+                            {props.details}
+                        </h2>
+                    </div>
+                </div>
+            </Link>
+        </li>
+    </>
+  )
+}
+
+export default FoodItem
